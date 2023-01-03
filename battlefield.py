7fd5444015e6abc47d3b68f8298a7bf5b0cc8c83
Wendy Homerding <wendy.homerding@gmail.com>
2023-01-03 10:44:12 -0500

    created dino and robot classes

--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,2 @@
-class Weapon:
-    def __init__(self, name, attack_power):
-        self.name = name
-        self.attach_power = attack_power
+
     
---
+++ weapon.py
@@ -0,0 +1,4 @@
+class Weapon:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attach_power = attack_power
