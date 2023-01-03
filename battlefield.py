a05bb5147db07e0d2fb4dce61c7c16b1236706ad
Wendy Homerding <wendy.homerding@gmail.com>
2023-01-03 10:57:43 -0500

    created battlefield class and outlined methods

--- battlefield.py
+++ battlefield.py
@@ -1,2 +1,18 @@
+from dinosaur import Dinosaur
+from robot import Robot
 
+class Battlefield:
+    def __init__(self):
+        pass
     
+    def run_game (self):
+        pass
+
+    def display_welcome (self):
+        pass
+
+    def battle_phase (self):
+        pass
+
+    def display_winner (self):
+        pass
